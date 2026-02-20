fca4a8b668f1a50d1ebb2752397b939fdc95ab23
x92120 <x92120@gmail.com>
2026-02-20 13:58:17 +0700

    feat: add translations.db, remove *.db from gitignore, set backend port to 8001

--- x02-BackEnd/x0201-fastAPI/main.py
+++ x02-BackEnd/x0201-fastAPI/main.py
@@ -115,5 +115,5 @@ def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8001))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
